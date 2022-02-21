7ff2fb1bfabddcbc77bab213e5d90041ceec2262
James Reed <reedandj1@gmail.com>
2022-02-21 08:14:50 +0000

    add media queries

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,11 @@
 import React from "react";
+import styles from "../styles/Result.module.css";
 
 function Result(props) {
   return (
     <p
       style={{ textAlign: "center", marginBottom: "5px", transition: "150ms" }}
+      className={styles.resultText}
     >
       The winner is {props.winner}!
     </p>
